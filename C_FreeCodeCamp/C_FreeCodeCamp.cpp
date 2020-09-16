d299bcfdddae9177a6f2bbd2049f1338622c3c78
Jad Driggers <jaddriggers@gmail.com>
2020-09-15 22:24:34 -0400

    Add project files.

---
+++ C_FreeCodeCamp/C_FreeCodeCamp.cpp
@@ -0,0 +1,38 @@
+
+// Building a better Calculator 
+// https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PL_z0e98vIDN_0zeN2UFglCAGyUyaLekm_&index=2&t=7569s
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	double num1;
+	double num2;
+	char op;
+
+	printf("Enter a number: ");
+	scanf_s("%lf", &num1);
+	printf("Enter operator: ");
+	scanf_s(" %c", &op, 1);
+	printf("Enter a number: ");
+	scanf_s("%lf", &num2);
+
+	if (op == '+') {
+		printf("%f", num1 + num2);
+	}
+	else if (op == '-') {
+		printf("%f", num1 - num2);
+	}
+	else if (op == '/') {
+		printf("%f", num1 / num2);
+	}
+	else if (op == '*') {
+		printf("%f", num1 * num2);
+	}
+	else {
+		printf("Invalid operator");
+	}
+	return 0;
+}
